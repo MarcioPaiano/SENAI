0ff9ca5b076cf8045277e525c0a7d0306c854f59
Marcio Paiano de Souza <41172555+MarcioPaiano@users.noreply.github.com>
2026-06-11 19:42:05 -0300

    Add files via upload

---
+++ projeto/data/final/criando pastats.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+pastas = [
+    "data/raw",
+    "data/processed/v1_com_outliers",
+    "data/processed/v2_outliers_tratado",
+    "data/final",
+    "notebooks",
+    "outputs/graficos"
+]
